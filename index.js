5c21e4e05941459f1da2c5ea611c18008b6e1afa
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-16 05:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function logWhisper(string){
   console.log(`${lcstring}`)
 }
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase)
+  if(string === string.toLowerCase)
     return "I can't hear you!";
+  if
   if(string === string.toUpperCase)
     return "Yes INDEED!";
   else (string === "I love you, Grandma")
